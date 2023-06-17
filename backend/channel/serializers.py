18c39d01a809b2fc6f0565760a8b887c9a073044
Thorium0 <troelscoolcraft@gmail.com>
2023-06-17 09:49:33 +0200

    added backend function to send messages

--- backend/channel/serializers.py
+++ backend/channel/serializers.py
@@ -12,4 +12,5 @@ class MessageSerializer(serializers.ModelSerializer):
                   'group',
                   'created',
                   'updated',
+                  'image'
                   )
